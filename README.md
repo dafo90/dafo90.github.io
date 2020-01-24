705509affbadb4adb7ee8a031501e88c3064e228
dafo90 <41897414+dafo90@users.noreply.github.com>
2020-01-24 23:15:06 +0100

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# dafo90.github.io
-My personal built with React, Material-UI, Redux and Saga
---
+++ precache-manifest.3f9d79408d4c354c7ecf7bd5d03e58b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a3d036d659f1b1ea5ed2f2229d6a8fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "538711216d98ed25b770",
+    "url": "/static/css/2.a641e29a.chunk.css"
+  },
+  {
+    "revision": "538711216d98ed25b770",
+    "url": "/static/js/2.d39ee8d9.chunk.js"
+  },
+  {
+    "revision": "df839a174b0dfd6afa9f3e95fd066433",
+    "url": "/static/js/2.d39ee8d9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c8707c7d02c7a48fece9",
+    "url": "/static/js/main.bfd9ca0c.chunk.js"
+  },
+  {
+    "revision": "f64f2826e6a738f7de53",
+    "url": "/static/js/runtime-main.0b89ac6d.js"
+  }
+]);
